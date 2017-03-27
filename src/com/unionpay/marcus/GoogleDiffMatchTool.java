6a7908f5e0dc681227ef67feb8aa783d7d2b87a3
Ji Ma <ji.ma@ndkey.com.cn>
2017-03-27 16:31:11 +0800

    init

---
+++ src/com/unionpay/marcus/GoogleDiffMatchTool.java
@@ -0,0 +1,16 @@
+package com.unionpay.marcus;
+
+/**
+ * Created by marcus on 17/3/27.
+ */
+public class GoogleDiffMatchTool implements fileDiffToolInterface {
+    @Override
+    public void diff(String filePath, String filePath2, String outputFileName) {
+
+    }
+
+    @Override
+    public void patch(String filePath, String outputFilePatch, String patchPath) {
+
+    }
+}
---
+++ src/com/unionpay/marcus/fileDiffToolInterface.java
@@ -0,0 +1,12 @@
+package com.unionpay.marcus;
+
+/**
+ * Created by marcus on 17/3/27.
+ */
+public interface fileDiffToolInterface {
+
+    void diff(String filePath, String filePath2, String outputFileName);
+
+    void patch(String filePath, String outputFilePatch, String patchPath);
+
+}
